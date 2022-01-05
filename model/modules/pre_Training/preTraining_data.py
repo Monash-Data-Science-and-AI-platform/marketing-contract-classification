327b2cc8dca423a8e49ee992cd6f96e0282efabc
isaacNingLee <ilee0022@student.monash.edu>
2022-01-05 10:09:09 +0800

    Change encoding to solve UTF encodings error

--- model/modules/pre_Training/preTraining_data.py
+++ model/modules/pre_Training/preTraining_data.py
@@ -14,7 +14,7 @@ class PreTraining_data:
       #method for extracting data from excel file
       #return df as pandas.DataFrame
       if paths[0].endswith('.csv'):
-        df=pd.read_csv(paths[0],dtype='string')
+        df=pd.read_csv(paths[0],dtype='string',encoding='Latin-1')
 
       elif paths[0].endswith('.xlsx'):
         df=pd.read_excel(paths[0],engine='openpyxl')
@@ -24,7 +24,7 @@ class PreTraining_data:
       for path in paths:#iterate across the remaning paths
         
         if path.endswith('.csv'):
-          new=pd.read_csv(path,dtype='string')
+          new=pd.read_csv(path,dtype='string',encoding='Latin-1')
 
         elif path.endswith('.xlsx'):
           new=pd.read_excel(path,engine='openpyxl')
