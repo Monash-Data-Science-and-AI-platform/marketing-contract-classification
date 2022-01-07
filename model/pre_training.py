faa6a46fa0b46162bf3979c045dd3e48ea9ed0a3
isaacNingLee <ilee0022@student.monash.edu>
2022-01-07 21:36:06 +0800

    Apply code to save model at the end of each loop

--- model/pre_training.py
+++ model/pre_training.py
@@ -36,6 +36,10 @@ loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)#input_ids
 optimizer = tf.keras.optimizers.Adam(preTraining['learning_rate'])
 model.compile(optimizer=optimizer,metrics=['accuracy'],loss=loss) # what loss function should I use???
 
-model.fit(train_dataset.shuffle(preTraining['shuffle']).batch(preTraining['batch_size']),epochs=preTraining['epochs'],batch_size=preTraining['batch_size'],callbacks=[WandbCallback()],verbose=2)
+for i in range(preTraining['epochs']):
+  model.fit(train_dataset.shuffle(preTraining['shuffle']).batch(preTraining['batch_size']),epochs=1,batch_size=preTraining['batch_size'],callbacks=[WandbCallback()],verbose=2)
 
-model.save_pretrained(preTraining['save_model_path'])
+  model.save_pretrained(preTraining['save_model_path'])
+
+  
+
