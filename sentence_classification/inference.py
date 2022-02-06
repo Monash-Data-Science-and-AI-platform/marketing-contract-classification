e02a4f0b1440b8cef5a3ffffb5ff623947ab1dea
isaacNingLee <ilee0022@student.monash.edu>
2022-02-06 10:02:36 +0800

    remove inconsistencies between fine tuning and inference

--- sentence_classification/inference.py
+++ sentence_classification/inference.py
@@ -37,8 +37,10 @@ outputs=model.predict(inputs.input_ids,batch_size=64)#get the predictions from t
 logits = outputs.logits#obtain the output logits
 print('Prediction done, processing the raw output\n')
 scaled_result=tf.math.sigmoid(logits).numpy()#scale the output logits with sigmoid function
-standardized_result=np.zeros(scaled_result.shape)#define the np array of the standarduzed result
-standardized_result[scaled_result>0.5]=1#standardized the scaled logits
+scaled_result[scaled_result>0.5]=1
+scaled_result[scaled_result<=0.5]=0
+
+standardized_result=scaled_result
 
 standardized_result_trans=np.transpose(standardized_result)#transpose the array for outputing the result
 
